cba9960e2a7bac87f00bd5adafb98e76f761de11
Justin Dray <justin@dray.be>
2025-11-10 18:29:50 -0800

    Initial commit

---
+++ test/cachedcurrencyrates.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cachedcurrencyrates from '../lib/cachedcurrencyrates-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cachedcurrencyrates-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cachedcurrencyrates.CachedcurrencyratesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
